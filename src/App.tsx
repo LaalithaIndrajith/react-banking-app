f9c0d48342f4fabf17698f8debd8ce911c9569f2
Laalitha <laalithagajanayake@gmail.com>
2024-10-02 22:17:45 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>Hello, Tailwind CSS with React and TypeScript!</h1>
+    </div>
+  );
+}
+
+export default App;
