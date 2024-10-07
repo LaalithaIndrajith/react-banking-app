93ccce8985e9f40c779dbfc5318a14c72ceacaba
Laalitha <laalithagajanayake@gmail.com>
2024-10-07 23:18:18 +0530

    Completed Hero Section Implementation
    - Finished the GetStarted component along with its styling.
    - Added a new homeParagText to the constants/index.ts file to simplify updating the wording in the Hero section.
    - Integrated the image with gradient effects in the Hero section.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -70,6 +70,10 @@ type Client = {
   logo: string;
 };
 
+export const homeParagText: string = `Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.`;
+
 export const navlinks: Navlink[] = [
   {
     id: "home",
